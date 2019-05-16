8024ad9d608c4aaf71f96ab821688b0c63dfa65c
Mantas Arlauskas <mantasarlauskas1997@gmail.com>
2019-05-16 13:42:05 +0300

    Added movie lists tests

--- src/components/movie-lists/movie-lists-container.js
+++ src/components/movie-lists/movie-lists-container.js
@@ -2,8 +2,7 @@ import { connect } from 'react-redux';
 import MovieLists from './movie-lists';
 
 const mapStateToProps = ({ lists: { array }, selectedMovie }) => ({
-  movieLists: array.filter(({ results }) => !!results.find(movie => movie.id === selectedMovie.id)),
-  selectedMovie
+  movieLists: array.filter(({ results }) => !!results.find(movie => movie.id === selectedMovie.id))
 });
 
 export default connect(mapStateToProps)(MovieLists);
