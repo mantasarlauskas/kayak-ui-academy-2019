4e8848400f49918d04b27b7208a574fa61363b84
Mantas Arlauskas <mantasarlauskas1997@gmail.com>
2019-05-16 14:50:47 +0300

    Fixed devtools issue

--- src/services/store.js
+++ src/services/store.js
@@ -4,7 +4,8 @@ import reducer from '../reducers';
 import initStorageClient from './local-storage-client';
 
 export default (initialState = {}) => {
-  const devMiddleware = typeof window !== 'undefined' ? window.devToolsExtension() : f => f;
+  const devMiddleware =
+    typeof window !== 'undefined' && window.devToolsExtension ? window.devToolsExtension() : f => f;
   const enhancedThunk = thunk.withExtraArgument({ storageClient: initStorageClient() });
   return createStore(
     reducer,
